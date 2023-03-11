6c23c9a2a392c42663648306080cfedd6ef1bf72
rienhyun <hrien.jin@gmail.com>
2023-03-11 20:01:17 +0900

    add Weight Cost for moving

---
+++ object/Path.py
@@ -0,0 +1,11 @@
+from object.Location import Location
+
+
+class Path:
+    def __init__(self, depart_loc: Location = None, arrive_loc: int = None, weight: int = 1):
+        self.depart_loc: Location = depart_loc
+        self.arrive_loc: Location = arrive_loc
+        self.weight: int = weight
+
+    def __str__(self):
+        return f"x: {self.arrive_loc.x}, y: {self.arrive_loc.y}"
